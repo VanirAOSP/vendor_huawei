2be1ab9b18a648e8f07193a117f02b04a5ed9245
nuclearmistake <nuclearmistake@gmail.com>
2015-12-01 02:23:41 -0500

    tinyxml: compile it
    Change-Id: Ief6f4b125939abd50f3dad8a712769c4a082b64b

--- angler/angler-vendor-blobs.mk
+++ angler/angler-vendor-blobs.mk
@@ -113,7 +113,6 @@ PRODUCT_COPY_FILES += \
     vendor/huawei/angler/proprietary/lib64/libloc_ds_api.so:system/lib64/libloc_ds_api.so \
     vendor/huawei/angler/proprietary/lib64/libloc_eng.so:system/lib64/libloc_eng.so \
     vendor/huawei/angler/proprietary/lib64/libManufacture.so:system/lib64/libManufacture.so \
-    vendor/huawei/angler/proprietary/lib64/libtinyxml.so:system/lib64/libtinyxml.so \
     vendor/huawei/angler/proprietary/lib/hw/camera.msm8994.so:system/lib/hw/camera.msm8994.so \
     vendor/huawei/angler/proprietary/lib/hw/gps.msm8994.so:system/lib/hw/gps.msm8994.so \
     vendor/huawei/angler/proprietary/lib/hw/nfc_nci.angler.so:system/lib/hw/nfc_nci.angler.so \
@@ -128,6 +127,5 @@ PRODUCT_COPY_FILES += \
     vendor/huawei/angler/proprietary/lib/libmmjpeg_interface.so:system/lib/libmmjpeg_interface.so \
     vendor/huawei/angler/proprietary/lib/libmm-qcamera.so:system/lib/libmm-qcamera.so \
     vendor/huawei/angler/proprietary/lib/libqomx_core.so:system/lib/libqomx_core.so \
-    vendor/huawei/angler/proprietary/lib/libtinyxml.so:system/lib/libtinyxml.so \
     vendor/huawei/angler/proprietary/lib/soundfx/libfmas.so:system/lib/soundfx/libfmas.so \
     vendor/huawei/angler/proprietary/xbin/wlutil:system/xbin/wlutil
