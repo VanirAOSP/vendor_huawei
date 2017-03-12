39cd2c147037e17e8f28acc866eb4f8aa17610c3
nuclearmistake <nuclearmistake@gmail.com>
2017-03-12 08:43:04 -0400

    Use more blobs and less compiled HW things - need libtime_genoff, chasing a unicorn with an initial 180degree error on gps lock w/ waze, and trying blindly to fix hotword
    Change-Id: I84d0251a39926ef90106c3ea7fd7716c0e9bb069

--- angler/angler-vendor.mk
+++ angler/angler-vendor.mk
@@ -22,33 +22,53 @@ PRODUCT_COPY_FILES += \
     vendor/huawei/angler/proprietary/etc/permissions/com.android.omadm.service.xml:system/etc/permissions/com.android.omadm.service.xml \
     vendor/huawei/angler/proprietary/etc/permissions/com.google.widevine.software.drm.xml:system/etc/permissions/com.google.widevine.software.drm.xml \
     vendor/huawei/angler/proprietary/etc/permissions/qcrilhook.xml:system/etc/permissions/qcrilhook.xml \
-    vendor/huawei/angler/proprietary/lib64/hw/nfc_nci.angler.so:system/lib64/hw/nfc_nci.angler.so \
-    vendor/huawei/angler/proprietary/lib64/libfilterpack_facedetect.so:system/lib64/libfilterpack_facedetect.so \
-    vendor/huawei/angler/proprietary/lib64/libManufacture.so:system/lib64/libManufacture.so \
+    vendor/huawei/angler/proprietary/lib/hw/audio.primary.msm8994.so:system/lib/hw/audio.primary.msm8994.so \
+    vendor/huawei/angler/proprietary/lib/hw/gps.msm8994.so:system/lib/hw/gps.msm8994.so \
     vendor/huawei/angler/proprietary/lib/hw/nfc_nci.angler.so:system/lib/hw/nfc_nci.angler.so \
     vendor/huawei/angler/proprietary/lib/libdmengine.so:system/lib/libdmengine.so \
     vendor/huawei/angler/proprietary/priv-app/DMService/lib/arm/libdmengine.so:system/priv-app/DMService/lib/arm/libdmengine.so \
     vendor/huawei/angler/proprietary/lib/libdmjavaplugin.so:system/lib/libdmjavaplugin.so \
     vendor/huawei/angler/proprietary/priv-app/DMService/lib/arm/libdmjavaplugin.so:system/priv-app/DMService/lib/arm/libdmjavaplugin.so \
     vendor/huawei/angler/proprietary/lib/libfilterpack_facedetect.so:system/lib/libfilterpack_facedetect.so \
-    vendor/huawei/angler/proprietary/lib/soundfx/libfmas.so:system/lib/soundfx/libfmas.so
+    vendor/huawei/angler/proprietary/lib/libgps.utils.so:system/lib/libgps.utils.so \
+    vendor/huawei/angler/proprietary/lib/libloc_core.so:system/lib/libloc_core.so \
+    vendor/huawei/angler/proprietary/lib/libloc_eng.so:system/lib/libloc_eng.so \
+    vendor/huawei/angler/proprietary/lib/libtinyxml.so:system/lib/libtinyxml.so \
+    vendor/huawei/angler/proprietary/lib/soundfx/libfmas.so:system/lib/soundfx/libfmas.so \
+    vendor/huawei/angler/proprietary/lib64/hw/audio.primary.msm8994.so:system/lib64/hw/audio.primary.msm8994.so \
+    vendor/huawei/angler/proprietary/lib64/hw/gps.msm8994.so:system/lib64/hw/gps.msm8994.so \
+    vendor/huawei/angler/proprietary/lib64/hw/nfc_nci.angler.so:system/lib64/hw/nfc_nci.angler.so \
+    vendor/huawei/angler/proprietary/lib64/libManufacture.so:system/lib64/libManufacture.so \
+    vendor/huawei/angler/proprietary/lib64/libfilterpack_facedetect.so:system/lib64/libfilterpack_facedetect.so \
+    vendor/huawei/angler/proprietary/lib64/libgps.utils.so:system/lib64/libgps.utils.so \
+    vendor/huawei/angler/proprietary/lib64/libloc_core.so:system/lib64/libloc_core.so \
+    vendor/huawei/angler/proprietary/lib64/libloc_eng.so:system/lib64/libloc_eng.so \
+    vendor/huawei/angler/proprietary/lib64/libtinyxml.so:system/lib64/libtinyxml.so
 
 PRODUCT_PACKAGES += \
     Tycho \
-    CarrierServices \
     CNEService \
+    CarrierServices \
     ConnMO \
     DCMO \
-    DiagMon \
     DMService \
+    DiagMon \
     GCS \
     HiddenMenu \
     HotwordEnrollment \
-    qcrilmsgtunnel \
     SprintDM \
+    qcrilmsgtunnel \
     com.google.widevine.software.drm \
     qcrilhook
+PRODUCT_COPY_FILES += \
+    vendor/huawei/angler/proprietary/lib/libimscamera_jni.so:system/lib/libimscamera_jni.so \
+    vendor/huawei/angler/proprietary/lib/libimsmedia_jni.so:system/lib/libimsmedia_jni.so \
+    vendor/huawei/angler/proprietary/lib64/libimscamera_jni.so:system/lib64/libimscamera_jni.so \
+    vendor/huawei/angler/proprietary/app/ims/lib/arm64/libimscamera_jni.so:system/app/ims/lib/arm64/libimscamera_jni.so \
+    vendor/huawei/angler/proprietary/lib64/libimsmedia_jni.so:system/lib64/libimsmedia_jni.so \
+    vendor/huawei/angler/proprietary/app/ims/lib/arm64/libimsmedia_jni.so:system/app/ims/lib/arm64/libimsmedia_jni.so
 
 PRODUCT_PACKAGES += \
-    ims \
-    TimeService
+    libtime_genoff \
+    TimeService \
+    ims
